6dd1702c5efd8a54e056a50a3e35ad8ac368d0ee
pgrzwt88bq-ai <pgrzwt88bq@privaterelay.appleid.com>
2026-05-10 23:44:51 +0100

    Update vendure-config.ts

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -4,15 +4,12 @@ import {
     DefaultSearchPlugin,
     VendureConfig,
 } from '@vendure/core';
-import { EmailPlugin } from '@vendure/email-plugin';
-import { AssetServerPlugin } from '@vendure/asset-server-plugin';
 import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-import path from 'path';
 
 export const config: VendureConfig = {
     apiOptions: {
-        port: process.env.PORT || 3000,
+        port: Number(process.env.PORT) || 3000,
         adminApiPath: 'admin-api',
         shopApiPath: 'shop-api',
     },
@@ -35,25 +32,8 @@ export const config: VendureConfig = {
         paymentMethodHandlers: [dummyPaymentHandler],
     },
     plugins: [
-        AssetServerPlugin.init({
-            route: 'assets',
-            assetUploadDir: path.join(__dirname, '../static/assets'),
-        }),
         DefaultJobQueuePlugin.init(),
         DefaultSearchPlugin.init(),
-        EmailPlugin.init({
-            handlers: [],
-            templatePath: path.join(__dirname, '../static/email/templates'),
-            globalTemplateVars: {
-                fromAddress: '"example" <noreply@example.com>',
-                verifyEmailAddressUrl: 'http://localhost:8080/verify',
-                passwordResetUrl: 'http://localhost:8080/password-reset',
-                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
-            },
-        }),
-        AdminUiPlugin.init({
-            route: 'admin',
-            port: 3002,
-        }),
+        AdminUiPlugin.init({ route: 'admin' }),
     ],
 };
